5e136d03e450f0bf61b945a4b88d6301a3bcd6f6
fer-andrade12 <fer.developer@outlook.com>
2022-04-22 13:33:01 -0300

    Projeto que simula uma calculadora criada em javascript

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+//função que chama os números e calcula
+function displaynum(n1 = parseFloat(text1.replace(",","."))){
+
+    
+    Calculator.text1.value = Calculator.text1.value + n1;
+
+   //document.text1.value = text1.value.toFixed(2);
+}
